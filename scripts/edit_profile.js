b236821cfd2c90e07cd45898363a0b09ed27a4de
Mark Michaely <markmichaely@gmail.com>
2022-04-14 20:39:08 +0300

    made scripts for form, close open and save

---
+++ scripts/edit_profile.js
@@ -0,0 +1,30 @@
+let popup = document.querySelector(".popup");
+let editBtn = document.querySelector(".profile-info__edit-button");
+let closeBtn = document.querySelector(".popup__close-btn");
+let nameInput = document.querySelector(".form__input_type_name");
+let jobInput = document.querySelector(".form__input_type_description");
+let profileName = document.querySelector(".profile-info__name");
+let profileDescription = document.querySelector(".profile-info__description");
+function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+  popup.classList.add("popup_opened");
+}
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+let formElement = document.querySelector(".form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  closePopup();
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+editBtn.addEventListener("click", openPopup);
+closeBtn.addEventListener("click", closePopup);
