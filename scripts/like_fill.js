e5f02a58515354afdd7f90996440fc6ef63b104c
mark <markmichaely@gmail.com>
2022-04-26 04:21:26 +0300

    removed unecessery files and comments

--- scripts/like_fill.js
+++
@@ -1,8 +0,0 @@
-let hearts = document.querySelectorAll(".card__like-btn");
-
-
-for (let index = 0; index < hearts.length; index++) {
-  hearts[index].addEventListener("click", function () {
-    hearts[index].classList.toggle("card__like-btn-filled");
-  });
-}
